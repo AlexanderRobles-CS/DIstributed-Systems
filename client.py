3deced9a2b958fde90af5ddb1a05d1f91201b51e
Logan Geivet <91626968+Geivet@users.noreply.github.com>
2023-05-04 12:02:09 -0700

    Update client.py

--- client.py
+++ client.py
@@ -6,27 +6,36 @@ from os import _exit
 from sys import stdout
 from time import sleep
 
-def lamportSort(pair):
-    return (-pair[0], -pair[1])
+def lampQueue(pair):            #function to help fix the lamport clock to queue them correctly
+    return (-pair[0], -pair[1]) #sorts by lamport clock, then by client number
 
-def mutexRequest(originalLamport):
-    sleep(3)
-    print("REQUEST <" + str(lamportClock) + ", " + str(clientNum) + ">", flush=True)
-    #lamportClock += 1
-    for client in outboundSOCKETS.values():
-        client.sendall(bytes(str(clientNum) + " request " + str(originalLamport), "utf-8"))
+
+# simulates network delay then handles received message
+def handle_msg(data):
+    sleep(3)                                # simulate 3 seconds message-passing delay
+    data = data.decode()                    # decode byte data into a string
+    print(data)                             # echo message to console
+
+
+def mutexReq(originalLamport):                                                               #function to send request to all clients
+    sleep(3)                                                                                 #sleep for 3 seconds to simulate the time it takes to send the request
+    #lamportClock += 1                                                                       #increment the lamport clock             
+    print("REQUEST <" + str(lamportClock) + ", " + str(clientNum) + ">", flush=True)         #print the request and the lamport clock
+    for client in outboundSOCKETS.values():                                                  #for each client in the outbound sockets
+        client.sendall(bytes(str(clientNum) + " request " + str(originalLamport), "utf-8"))  #send the request to the client
         # print("Request sent to client " + str(client), flush=True)
-        sleep(1)
+        sleep(1)                                                                             #sleep for 1 second
 
-def mutexRelease(originalLamport):
-    sleep(3)
-    #lamportClock += 1
-    print("RELEASE <" + str(lamportClock) + ", " + str(clientNum) + ">", flush=True)
-    for client in outboundSOCKETS.values():
-        client.sendall(bytes(str(clientNum) + " release " + str(lamportClock) + " " + str(originalLamport), "utf-8"))
+
+def mutexRel(originalLamport):
+    sleep(3)                                                                                                            #sleep for 3 seconds to simulate the time it takes to send the release
+    #lamportClock += 1                                                                                                  #increment the lamport clock              
+    print("RELEASE <" + str(lamportClock) + ", " + str(clientNum) + ">", flush=True)                                    #print the release and the lamport clock
+    for client in outboundSOCKETS.values():                                                                             #for each client in the outbound sockets
+        client.sendall(bytes(str(clientNum) + " release " + str(lamportClock) + " " + str(originalLamport), "utf-8"))   #send the release to the client
        #  print("Release sent to client " + str(client), flush=True)
-        print("DONE <" + str(lamportClock) + ", " + str(clientNum) + ">", flush=True)
-        sleep(1)
+        print("DONE <" + str(lamportClock) + ", " + str(clientNum) + ">", flush=True)                                   #print the done and the lamport clock
+        sleep(1)                                                                                                        #sleep for 1 second
     
 
 # keep waiting and asking for user inputs
@@ -45,52 +54,57 @@ def get_user_input():
             sleep(int(sleepNum))
 
         else: 
-            if waitFunc[0] == "Transfer":
-                transferTarget = waitFunc[1]
-                transferTarget = transferTarget.strip("P")
+            if waitFunc[0] == "Transfer":                                 #if the user input is a transfer
+                transferTarget = waitFunc[1]                              #set transfer target to the second word in the user input to get the client number
+                transferTarget = transferTarget.strip("P")                #strip the P from the transfer target so we only have the number
 
-                global lamportClock
-                lamportClock += 1
+                global lamportClock                                        #global variable for the lamport clock
+                lamportClock += 1                                          #increment the lamport clock
 
-                originalLamport = lamportClock
+                originalLamport = lamportClock                             #save the original lamport clock
 
-                print("REQUEST <" + str(lamportClock) + ", " + str(clientNum) + ">", flush=True)
+                print("REQUEST <" + str(lamportClock) + ", " + str(clientNum) + ">", flush=True)     #print the request and the lamport clock as designated in the instructions
 
-                queue.append((lamportClock, int(clientNum)))
-                queue.sort(key=lamportSort)
+                mutexQueue.append((lamportClock, int(clientNum)))          #append the lamport clock and the client number to the mutex queue
+                mutexQueue.sort(key=lampQueue)                             #sort the mutex queue by the lamport clock and the client number
 
-                mutexRequest(originalLamport)
-                timeoutCount = 0
+                mutexReq(originalLamport)                                  #call the mutex request function with the original lamport clock value
+                
+
+                #while loop to allow for timeout in case of client failure
+                timeoutVal = 0                                             #set the timeout value to 0
                 while (ReplyArr[0] != 1 and ReplyArr[1] != 1) or (ReplyArr[0] != 1 and ReplyArr[2] != 1) or (ReplyArr[1] != 1 and ReplyArr[2] != 1):
-                    timeoutCount += 1
-                    sleep(1)
-                    if timeoutCount == 9:
-                        print("INCORRECT")
-                        break
+                    timeoutVal += 1                                #increment the timeout value
+                    sleep(1)                                       #sleep for 1 second
+                    if timeoutVal == 9:                            #if the timeout value reaches 9
+                        print("INCORRECT")                         #print incorrect       
+                        break                                      #break out of the loop
                     continue
                 
-                print("QUEUE: " , str(queue))
+                print("QUEUE: " , str(mutexQueue))                 #print the mutex queue
                 
-                while(queue[0][0] != int(lamportClock)) and (queue[0][1] != int(clientNum)):
-                    # print("queue[0][0]: " + str(queue[0][0]) + " lamportClock: " + str(lamportClock) + " queue[0][1]: " + str(queue[0][1]) + " transferTarget: " + str(transferTarget))
+                #while loop to wait until the mutex queue head is the same as the current client
+                while(mutexQueue[0][0] != int(lamportClock)) and (mutexQueue[0][1] != int(clientNum)):  #while the lamport clock and client number in the mutex queue are not equal to the lamport clock and client number
+                    # print("mutexQueue[0][0]: " + str(mutexQueue[0][0]) + " lamportClock: " + str(lamportClock) + " mutexQueue[0][1]: " + str(mutexQueue[0][1]) + " transferTarget: " + str(transferTarget))
                     continue
                 
-                out_sock.sendall(bytes(user_input + " <" + str(originalLamport) + "," + str(clientNum) + ">", "utf-8"))
-                lamportClock += 1
+                out_sock.sendall(bytes(user_input + " <" + str(originalLamport) + "," + str(clientNum) + ">", "utf-8")) #send the user input to the server with the original lamport clock and client number
+                lamportClock += 1                                                                                      #increment the lamport clock
 
-                queue.pop(0)
-                ReplyArr[0] = 0
-                ReplyArr[1] = 0
-                ReplyArr[2] = 0
+                mutexQueue.pop(0)                                                                                   #pop the head of the mutex queue
+                #reset the reply array so the replies can be counted again for a new transaction
+                ReplyArr[0] = 0                                                                                     #set the reply[0] array to 0
+                ReplyArr[1] = 0                                                                                     #set the reply[1] array to 0
+                ReplyArr[2] = 0                                                                                     #set the reply[2] array to 0
 
-                lamportClock += 1
-                mutexRelease(originalLamport)
+                lamportClock += 1                                                                                   #increment the lamport clock
+                mutexRel(originalLamport)                                                                           #call the mutex release function with the original lamport clock value
 
-                print("SUCCESS")
+                print("SUCCESS")                                                                                    #print success for a successful transaction
 
-            if waitFunc[0] == "Balance":
-                out_sock.sendall(bytes(user_input, "utf-8"))
-                lamportClock += 1
+            if waitFunc[0] == "Balance":                                                #if the user input is a balance
+                out_sock.sendall(bytes(user_input, "utf-8"))                            #send the user input to the server
+                lamportClock += 1                                                       #increment the lamport clock
 
             else:
                 try:
@@ -99,11 +113,6 @@ def get_user_input():
                     print("Exception in sending to server")
                 continue
 
-# simulates network delay then handles received message
-def handle_msg(data):
-    sleep(3)                                # simulate 3 seconds message-passing delay
-    data = data.decode()                    # decode byte data into a string
-    print(data)                             # echo message to console
 
 def getConnections():
     while True:                              # while loop to make new connections
@@ -116,6 +125,7 @@ def getConnections():
         print("New connection from inbound client", flush=True)
 
         threading.Thread(target=listenForClients, args=(conn,addr)).start()
+
 
 def respond(data):                            # handle a new connection by waiting to receive from connection
     global lamportClock
@@ -131,8 +141,8 @@ def respond(data):                            # handle a new connection by waiti
     if msg == "request":
         lamportClock = 1 + max(lamportClock, incomingLamport)
 
-        queue.append((incomingLamport, incomingID))
-        queue.sort(key=lamportSort)
+        mutexQueue.append((incomingLamport, incomingID))
+        mutexQueue.sort(key=lampQueue)
 
         lamportClock += 1
 
@@ -149,8 +159,9 @@ def respond(data):                            # handle a new connection by waiti
         incomingLamport = int(data[3])
         lamportClock = 1 + max(lamportClock, incomingLamport)
         print("Client P" + str(incomingID) + " released <" + str(incomingLamport) + "," + str(clientNum) + ">", flush=True)
-        queue.pop(0)
+        mutexQueue.pop(0)
     
+
 def listenForClients(conn, addr):
     while True:
         try:
@@ -168,20 +179,20 @@ def listenForClients(conn, addr):
 
 
 if __name__ == "__main__":
-    sleep(1)                                # decode byte data into a string
-    queue = []
-    lamportClock = 0
-    ReplyArr = [0,0,0]
+    sleep(1)                                # sleep for 1 second to allow server to start up
+    mutexQueue = []                         # initialize queue for mutex
+    lamportClock = 0                        # initialize lamport clock
+    ReplyArr = [0,0,0]                      # initialize reply array
 
     # ------------------ server socket setup ------------------ #
-    SERVER_IP = socket.gethostname()
-    SERVER_PORT = 9000
+    SERVER_IP = socket.gethostname()        # get server IP address
+    SERVER_PORT = 9000                      # server port set to 9000
 
-    pid = str(sys.argv[1])
+    pid = str(sys.argv[1])                                          #set pid to command line argument
     out_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    # create a socket object, SOCK_STREAM specifies a TCP socket
     out_sock.connect((SERVER_IP, SERVER_PORT))                      # attempt to connect own socket to server's socket address
-    print("Client " + pid + " has connected to the server.", flush=True)
-    out_sock.sendall(bytes(pid, "utf-8"))
+    print("Client " + pid + " has connected to the server.", flush=True)    # print confirmation of connection to server
+    out_sock.sendall(bytes(pid, "utf-8"))                                   # send pid to server
 
     #-----------------------------------------------------------#
         
@@ -192,67 +203,67 @@ if __name__ == "__main__":
     #---------------------------------------------------------------------#
 
     #---------------------- client socket setup ----------------------#
-    CLIENT_IP = socket.gethostname()
+    CLIENT_IP = socket.gethostname()                                        # get client IP address
     CLIENT_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)       # create a socket object, SOCK_STREAM specifies a TCP socket
-    CLIENT_SOCKET.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    CLIENT_SOCKET.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)     # set socket option to allow reuse of address
     CLIENT_SOCKET.bind((CLIENT_IP, CLIENT_PORT))                            # bind socket to address
     CLIENT_SOCKET.listen()                                                  # start listening for connections to the address
     CLIENT_SOCKETS = []                                                     # container to store all connections
 
     #-----------------------------------------------------------------#
 
-    threading.Thread(target=getConnections).start()
+    threading.Thread(target=getConnections).start()                         # start thread to listen for new connections
 
     # ------------------ outbound socket setup ------------------ #
-    sleep(5)
-    outboundSOCKETS = {}
+    sleep(5)                                                            # sleep for 5 seconds to allow all clients to connect to server
+    outboundSOCKETS = {}                                                # create dictionary to store outbound sockets
     outBoundSocketA = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create a socket object, SOCK_STREAM specifies a TCP socket
     outBoundSocketB = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create a socket object, SOCK_STREAM specifies a TCP socket
 
     #-------------------------------------------------------------#
 
     #----------- bind sockets using port number ------------#
-    if clientNum == "1":
-        outBoundSocketA.connect((SERVER_IP, 9002))
-        outboundSOCKETS[2] = outBoundSocketA
-        print("Connected to client P2", flush=True)
+    if clientNum == "1":                                # if client number is 1
+        outBoundSocketA.connect((SERVER_IP, 9002))      # connect to client 2 with port number 9002
+        outboundSOCKETS[2] = outBoundSocketA            # store socket in dictionary with key 2
+        print("Connected to client P2", flush=True)     # print confirmation of connection to client 2
         
-        outBoundSocketB.connect((SERVER_IP, 9003))
-        outboundSOCKETS[3] = outBoundSocketB
-        print("Connected to client P3", flush=True)
+        outBoundSocketB.connect((SERVER_IP, 9003))      # connect to client 3 with port number 9003
+        outboundSOCKETS[3] = outBoundSocketB            # store socket in dictionary with key 3
+        print("Connected to client P3", flush=True)     # print confirmation of connection to client 3
 
-    if clientNum == "2":
-        outBoundSocketA.connect((SERVER_IP, 9001))
-        outboundSOCKETS[1] = outBoundSocketA
-        print("Connected to client P1", flush=True)
+    if clientNum == "2":                                # if client number is 2
+        outBoundSocketA.connect((SERVER_IP, 9001))      # connect to client 1 with port number 9001
+        outboundSOCKETS[1] = outBoundSocketA            # store socket in dictionary with key 1
+        print("Connected to client P1", flush=True)     # print confirmation of connection to client 1
         
-        outBoundSocketB.connect((SERVER_IP, 9003))
-        outboundSOCKETS[3] = outBoundSocketB
-        print("Connected to client P3", flush=True)
+        outBoundSocketB.connect((SERVER_IP, 9003))      # connect to client 3 with port number 9003
+        outboundSOCKETS[3] = outBoundSocketB            # store socket in dictionary with key 3
+        print("Connected to client P3", flush=True)     # print confirmation of connection to client 3
     
-    if clientNum == "3":
-        outBoundSocketA.connect((SERVER_IP, 9001))
-        outboundSOCKETS[1] = outBoundSocketA
-        print("Connected to client P1", flush=True)
+    if clientNum == "3":                                # if client number is 3
+        outBoundSocketA.connect((SERVER_IP, 9001))      # connect to client 1 with port number 9001
+        outboundSOCKETS[1] = outBoundSocketA            # store socket in dictionary with key 1
+        print("Connected to client P1", flush=True)     # print confirmation of connection to client 1
         
-        outBoundSocketB.connect((SERVER_IP, 9002))
-        outboundSOCKETS[2] = outBoundSocketB
-        print("Connected to client P2", flush=True)
+        outBoundSocketB.connect((SERVER_IP, 9002))      # connect to client 2 with port number 9002
+        outboundSOCKETS[2] = outBoundSocketB            # store socket in dictionary with key 2
+        print("Connected to client P2", flush=True)     # print confirmation of connection to client 2
     #-------------------------------------------------------#
 
-    threading.Thread(target=get_user_input).start()
+    threading.Thread(target=get_user_input).start()     # start thread to get user input
 
-    while True:
+    while True:                                         # loop to listen for incoming messages
         try:
-            data = out_sock.recv(1024)
+            data = out_sock.recv(1024)                  # receive data from server
 
         except:
-            print("Exception in receiving")
+            print("Exception in receiving")             # print exception message
             break
-        if not data:
-            out_sock.close()
-            print("Connection closed from server")
+        if not data:                                    # if no data is received
+            out_sock.close()                            # close socket
+            print("Connection closed from server")      # print confirmation of connection to server
             break
 
-    threading.Thread(target=handle_msg, args=(data,)).start()
+    threading.Thread(target=handle_msg, args=(data,)).start()   # start thread to handle incoming message
 
