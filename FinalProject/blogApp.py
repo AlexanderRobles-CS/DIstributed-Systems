b8eb695db01d11bb1f5ec99f78f4a6ffe934eaea
Geivet <lgeivet@ucsb.edu>
2023-06-11 13:59:48 -0700

    test w comments

--- FinalProject/blogApp.py
+++ FinalProject/blogApp.py
@@ -1,9 +1,9 @@
 class Blog:
     def __init__(self):                         # container for blog posts
-        self.blogBin = {}
+        self.blogBin = {}                       # dictionary of blogs
 
     def commitPost(self, operation, user, title, contents):        # add post to blog
-        if user not in self.blogBin: 
-            self.blogBin[user] = []
+        if user not in self.blogBin:                        # if user doesn't have a blog yet
+            self.blogBin[user] = []                         # create one
 
-        self.blogBin[user].append((operation, title, contents))
+        self.blogBin[user].append((operation, title, contents))   # add post to blog
